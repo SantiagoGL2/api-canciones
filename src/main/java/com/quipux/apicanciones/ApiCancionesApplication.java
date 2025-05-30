6febc4e346bb11e35f1df2c3a20a504a7ff000c9
Santiago Gutierrez <santiago.gutierrez@onnovacion.com>
2025-05-29 22:16:23 -0500

    chore(init): configuración inicial de la estructura base de la API

---
+++ src/main/java/com/quipux/apicanciones/ApiCancionesApplication.java
@@ -0,0 +1,14 @@
+package com.quipux.apicanciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class ApiCancionesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCancionesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quipux/apicanciones/ApiCancionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quipux.apicanciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCancionesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
